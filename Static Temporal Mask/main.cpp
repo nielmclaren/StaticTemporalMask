8d9dffe00a8a9415e9ec5a29b7fdf16bd655f995
danielgm <dan@danielmclaren.com>
2012-12-02 22:24:03 -0800

    Initial Commit

---
+++ Static Temporal Mask/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Static Temporal Mask
+//
+//  Created by Daniel McLaren on 12-12-02.
+//  Copyright (c) 2012 Daniel McLaren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
